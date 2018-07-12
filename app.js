c8a27d6b7417750323fd4bddb0e6e694d830ee8a
Tate Walker <tatewalker@tamu.edu>
2018-07-12 00:25:47 -0500

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const Discord = require("discord.js");
 var schedule = require('node-schedule');
+var port = process.env.PORT || 8080;
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
